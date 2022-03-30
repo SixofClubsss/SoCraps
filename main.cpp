de6df20ddd6e4756a49b3a7040dc4e40c2248db5
SixofClubs <84689659+SixofClubsss@users.noreply.github.com>
2022-03-30 13:26:22 -0600

    Updated main.cpp
    Added in rulespopup window to show up when SoCraps is first started.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    rulespopup rulespopup;
+    rulespopup.setModal(true);
+    rulespopup.exec();
     MainWindow w;
     w.show();
     return a.exec();
